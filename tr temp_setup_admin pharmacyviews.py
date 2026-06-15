9e36b03aa2df9721f08277b190b981f16bb7f5b4
mr.break <xgadjet192@gmail.com>
2026-06-15 09:56:49 +0330

    debug setup admin v2

---
+++ tr temp_setup_admin pharmacyviews.py
@@ -0,0 +1,5 @@
+[33mce1e02a[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m remove temp setup admin
+[33m9b52622[m add groq to requirements
+[33m3ec9f1b[m add temp_setup_admin view
+[33m328ae49[m add temp setup admin url
+[33m7ebfec8[m Merge branch 'main' of https://github.com/xgadjet192-web/pharmacy-mohammadi
